2191d3dca85ab533c789e42de2eec93bf101e71d
procode47 <iloenyenwavictor@gmail.com>
2023-02-14 00:27:28 +0100

    debug

--- server/index.js
+++ server/index.js
@@ -136,25 +136,25 @@ app.post("/tweet", async (req, res) => {
       .order("id", { ascending: false })
       .eq("login", author)
       .limit(1);
-      if (data[0].refresh){
-        const refresh = data[0].refresh;
-        const { client, accessToken, refreshToken } =
-          await twitterClient.refreshOAuth2Token(refresh);
-        const { err } = await supabase
-          .from("tokens")
-          .update({ access: accessToken, refresh: refreshToken })
-          .eq("login", author);
-        // console.log(data);
-        if (tweet.includes("tweet:")) {
-          const updatedTweet = tweet.replace("tweet:", "");
-          const { data: tweetData } = await client.v2.tweet(
-            `#automatedbystreakbot \n ${updatedTweet} \n ${link}`
-          );
-          console.log("tweet successful");
-        }
-      } else {
-        console.log("No Twitter Keys Found")
+    if (data[0].refresh) {
+      const refresh = data[0].refresh;
+      const { client, accessToken, refreshToken } =
+        await twitterClient.refreshOAuth2Token(refresh);
+      const { err } = await supabase
+        .from("tokens")
+        .update({ access: accessToken, refresh: refreshToken })
+        .eq("login", author);
+      // console.log(data);
+      if (tweet.includes("tweet:")) {
+        const updatedTweet = tweet.replace("tweet:", "");
+        const { data: tweetData } = await client.v2.tweet(
+          `#automatedbystreakbot \n ${updatedTweet} \n ${link}`
+        );
+        console.log("tweet successful");
       }
+    } else {
+      console.log("No Twitter Keys Found");
+    }
   }
 });
 
@@ -162,7 +162,16 @@ app.post("/tweet", async (req, res) => {
 app.get("/tweet/v2", async (req, res) => {
   const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
     callbackURL,
-    { scope: ["tweet.read", "tweet.write", "users.read", "offline.access","like.write"] }
+    {
+      scope: [
+        "tweet.read",
+        "tweet.write",
+        "users.read",
+        "offline.access",
+        "like.write",
+        "like.read"
+      ],
+    }
   );
   res.redirect(url);
   // console.log({ codeVerifier, state });
